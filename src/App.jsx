923824a2ea44ea6c96a704d28accdeee0140a633
khalid192 <karazani2003@gmail.com>
2026-02-25 23:21:10 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import SEARCHE from "./SEARCHE.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Container
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card
+          sx={{
+            height: "80vh",
+            width: "60vw",
+            padding: 0,
+          }}
+        >
+          <Typography
+            variant="h6"
+            gutterBottom
+            style={{
+              textAlign: "center",
+              my: 2,
+              padding: "5px 0px",
+              background: "linear-gradient(135deg,#7ea0e9,#133172)",
+              color: "#fff",
+              fontWeight: "bolder",
+            }} 
+          >
+            Smart Users Dashboard
+          </Typography>
+          <></>
+          <SEARCHE/>
 
+
+        </Card>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
